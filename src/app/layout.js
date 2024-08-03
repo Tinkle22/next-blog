2ae35ae33b8caee09650665b01992f0413967f8d
Tinkle22 <104595797+Tinkle22@users.noreply.github.com>
2024-08-03 18:13:38 +0200

    started project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,27 @@
+import NavBar from '@/components/NavBar/NavBar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
+  title: 'CM traders',
   description: 'The best blog app!',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            <NavBar/>
+              {children}
+            <Footer/>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
